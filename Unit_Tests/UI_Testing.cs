b3907a66cc448359a6cf45d9c69db19d8d4d0415
JeffreyMJohnson <jefjohms@gmail.com>
2015-05-09 15:48:32 -0700

    Have File-NewSheet Not empty unit test complete.

--- Unit_Tests/UI_Testing.cs
+++ Unit_Tests/UI_Testing.cs
@@ -144,13 +144,154 @@ namespace Unit_Tests
         }
 
         /// <summary>
+        /// verify the sheet clears, scollviewer background is green and no alert message pops
+        /// </summary>
+        [TestMethod]
+        public void NewSheetWithEmpty()
+        {
+            ClickFileMenuItem();
+            AutomationElement fileNewMenuItem = GetElement("menuItemNewSheet");
+
+            //verify
+            
+
+        }
+
+        [TestMethod]
+        public void NewSheetNotEmpty()
+        {
+            //ClickFileMenuItem();
+            for (int i = 0; i < 5; i++)
+            {
+                LoadImage(PROJECT_PATH + @"resources\test_images\small\", "green_square_small.png");
+            }
+            //click file-new
+            ClickFileMenuItem();
+            AutomationElement fileNew = GetElement("menuItemNewSheet");
+            InvokePattern fileNewPat = (InvokePattern)fileNew.GetCurrentPattern(InvokePattern.Pattern);
+            fileNewPat.Invoke();
+
+            //verify main window still there
+            //mainWindow = desktop.FindFirst(TreeScope.Children, new PropertyCondition(AutomationElement.NameProperty, "Atlas Engine"));
+            //Assert.IsNotNull(mainWindow);
+
+            //check for pop up window
+            AutomationElement popUp = mainWindow.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.NameProperty, "Are You Sure?"));
+            Assert.IsNotNull(popUp);
+
+            //click yes button
+            AutomationElement btnYes = popUp.FindFirst(TreeScope.Children, new PropertyCondition(AutomationElement.NameProperty, "Yes"));
+            Assert.IsNotNull(btnYes);
+
+            InvokePattern btnPattern = (InvokePattern)btnYes.GetCurrentPattern(InvokePattern.Pattern);
+            btnPattern.Invoke();
+
+            //verify default settings
+            AutomationElement txtWidthReadOnly = GetElement("txtWidthReadOnly");
+            AutomationElement txtWidthWrite = GetElement("txtWidthWrite");
+            AutomationElement txtHeightReadOnly = GetElement("txtHeightReadOnly");
+            AutomationElement txtHeightWrite = GetElement("txtHeightWrite");
+
+            //verify widthRO is not visible and widthWrite is
+            Assert.IsFalse((bool)txtWidthWrite.GetCurrentPropertyValue(AutomationElement.IsOffscreenProperty));
+            Assert.IsTrue((bool)txtWidthReadOnly.GetCurrentPropertyValue(AutomationElement.IsOffscreenProperty));
+
+            //verify heightRO is not visible and heightWrite is
+            Assert.IsFalse((bool)txtHeightWrite.GetCurrentPropertyValue(AutomationElement.IsOffscreenProperty));
+            Assert.IsTrue((bool)txtHeightReadOnly.GetCurrentPropertyValue(AutomationElement.IsOffscreenProperty));
+
+            //can't check canvas size now need to refactor
+
+            //verify the value
+            ValuePattern txtWidthValue = (ValuePattern)txtWidthWrite.GetCurrentPattern(ValuePattern.Pattern);
+            ValuePattern txtHeightValue = (ValuePattern)txtHeightWrite.GetCurrentPattern(ValuePattern.Pattern);
+            int width = Int32.Parse(txtWidthValue.Current.Value);
+            int height = Int32.Parse(txtHeightValue.Current.Value);
+
+            Assert.AreEqual(DEFAULT_WIDTH, width);
+            Assert.AreEqual(DEFAULT_HEIGHT, height);
+
+
+        }
+
+        private void LoadImage(string path, string imageFile)
+        {
+            //click file
+            ClickFileMenuItem();
+            //click load image
+            AutomationElement loadImageItem = GetElement("menuItemLoadImage");
+            InvokePattern loadImgPattern = (InvokePattern)loadImageItem.GetCurrentPattern(InvokePattern.Pattern);
+            loadImgPattern.Invoke();
+            //get file select window - located as child of app window but no automationId, so have to use name
+            AutomationElement fileOpenWindow = null;
+
+            //might take a bit so need to loop
+            int numWaits = 0;
+            do
+            {
+                Console.WriteLine("Looking for file open dialog...");
+                fileOpenWindow = mainWindow.FindFirst(TreeScope.Children, new PropertyCondition(AutomationElement.NameProperty, "Open"));
+                ++numWaits;
+                Thread.Sleep(200);
+            } while (null == fileOpenWindow);
+            if (null == fileOpenWindow)
+            {
+                throw new Exception("Could not find file open window");
+            }
+            else
+            {
+                Console.WriteLine("Found file open window");
+            }
+
+            //get textbox for file
+            AutomationElement txtFileSelect = null;
+
+            AutomationElementCollection elems = fileOpenWindow.FindAll(TreeScope.Descendants, new PropertyCondition(AutomationElement.NameProperty, "File name:"));
+            foreach (AutomationElement elem in elems)
+            {
+                //check class name
+                if (elem.Current.ClassName == "Edit")
+                {
+                    txtFileSelect = elem;
+                }
+            }
+
+            Assert.IsNotNull(txtFileSelect);
+
+            //get pattern
+            ValuePattern txtValue = (ValuePattern)txtFileSelect.GetCurrentPattern(ValuePattern.Pattern);
+            //set it for path
+            Console.WriteLine("Loading file: " + path + imageFile);
+            txtValue.SetValue(path + imageFile);
+
+            //get OK button 
+            AutomationElement openButton = null;
+            openButton = fileOpenWindow.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.AutomationIdProperty, "1"));
+            Assert.IsNotNull(openButton);
+            //click it
+            InvokePattern openButtonPattern = (InvokePattern)openButton.GetCurrentPattern(InvokePattern.Pattern);
+            openButtonPattern.Invoke();
+        }
+
+
+        /// <summary>
         /// Helper function for finding elements in window
         /// </summary>
         /// <param name="automationIdProperty"></param>
         /// <returns></returns>
         private AutomationElement GetElement(string automationIdProperty)
         {
-            return mainWindow.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.AutomationIdProperty, automationIdProperty));
+            AutomationElement result = null;
+            result = mainWindow.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.AutomationIdProperty, automationIdProperty));
+            if (null == result)
+            {
+                throw new Exception("Could not find element: " + automationIdProperty);
+            }
+            else
+            {
+                Console.WriteLine("Found element: " + automationIdProperty);
+            }
+            return result;
         }
 
         private void StartProgram()
@@ -218,15 +359,7 @@ namespace Unit_Tests
 
         private void ClickFileMenuItem()
         {
-            fileMenuItem = mainWindow.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.AutomationIdProperty, "menuItemFile"));
-            if (null == fileMenuItem)
-            {
-                throw new Exception("Could not find file menu control.");
-            }
-            else
-            {
-                Console.WriteLine("File menu control found.");
-            }
+            fileMenuItem = GetElement("menuItemFile");
             Console.WriteLine("Clicking file on menu");
             ExpandCollapsePattern clickFile = (ExpandCollapsePattern)fileMenuItem.GetCurrentPattern(ExpandCollapsePattern.Pattern);
             clickFile.Expand();
@@ -236,17 +369,7 @@ namespace Unit_Tests
         {
             ClickFileMenuItem();
             Console.WriteLine("Clicking File-Exit");
-            AutomationElement fileExit = null;
-            fileExit = mainWindow.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.NameProperty, "Exit Program"));
-            if (null == fileExit)
-            {
-                throw new Exception("File-Exit not found.");
-            }
-            else
-            {
-                Console.WriteLine("File-Exit control found.");
-            }
-
+            AutomationElement fileExit = GetElement("menuItemExitProgram");
             InvokePattern clickFileExit = (InvokePattern)fileExit.GetCurrentPattern(InvokePattern.Pattern);
             clickFileExit.Invoke();
         }
